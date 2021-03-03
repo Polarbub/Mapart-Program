644908e996cefd13a9c1752728f4e3cf2f5cf092
polarbub <bryn.hakl@gmail.com>
2021-03-03 17:28:52 -0600

    started trying to figure out the libarary

--- Mapart.cpp
+++ Mapart.cpp
@@ -1,6 +1,136 @@
 #include <iostream>
 #include <nbt.h>
+#define NBT_IMPLEMENTATION
+
+static size_t reader_read(void* userdata, uint8_t* data, size_t size) {
+  return fread(data, 1, size, userdata);
+}
+
+static size_t writer_write(void* userdata, uint8_t* data, size_t size) {
+  return fwrite(data, 1, size, userdata);
+}
+
+static void print_nbt_tree(nbt_tag_t* tag, int indentation) {
+  for (int i = 0; i < indentation; i++) {
+    printf(" ");
+  }
+
+  if (tag->name) {
+    printf("%s: ", tag->name);
+  }
+
+  switch (tag->type) {
+    case NBT_TYPE_END: {
+      printf("[end]");
+      break;
+    }
+    case NBT_TYPE_BYTE: {
+      printf("%hhd", tag->tag_byte.value);
+      break;
+    }
+    case NBT_TYPE_SHORT: {
+      printf("%hd", tag->tag_short.value);
+      break;
+    }
+    case NBT_TYPE_INT: {
+      printf("%d", tag->tag_int.value);
+      break;
+    }
+    case NBT_TYPE_LONG: {
+      printf("%ld", tag->tag_long.value);
+      break;
+    }
+    case NBT_TYPE_FLOAT: {
+      printf("%f", tag->tag_float.value);
+      break;
+    }
+    case NBT_TYPE_DOUBLE: {
+      printf("%f", tag->tag_double.value);
+      break;
+    }
+    case NBT_TYPE_BYTE_ARRAY: {
+      printf("[byte array]");
+      break;
+      for (size_t i = 0; i < tag->tag_byte_array.size; i++) {
+        printf("%hhd ", tag->tag_byte_array.value[i]);
+      }
+      break;
+    }
+    case NBT_TYPE_STRING: {
+      printf("%s", tag->tag_string.value);
+      break;
+    }
+    case NBT_TYPE_LIST: {
+      printf("\n");
+      for (size_t i = 0; i < tag->tag_list.size; i++) {
+        print_nbt_tree(tag->tag_list.value[i], indentation + tag->name_size + 2);
+      }
+      break;
+    }
+    case NBT_TYPE_COMPOUND: {
+      printf("\n");
+      for (size_t i = 0; i < tag->tag_compound.size; i++) {
+        print_nbt_tree(tag->tag_compound.value[i], indentation + tag->name_size + 2);
+      }
+      break;
+    }
+    case NBT_TYPE_INT_ARRAY: {
+      printf("[int array]");
+      break;
+      for (size_t i = 0; i < tag->tag_int_array.size; i++) {
+        printf("%d ", tag->tag_int_array.value[i]);
+      }
+      break;
+    }
+    case NBT_TYPE_LONG_ARRAY: {
+      printf("[long array]");
+      break;
+      for (size_t i = 0; i < tag->tag_long_array.size; i++) {
+        printf("%ld ", tag->tag_long_array.value[i]);
+      }
+      break;
+    }
+    default: {
+      printf("[error]");
+    }
+  }
+
+  printf("\n");
+}
+
+nbt_tag_t* read_nbt_file(const char* name, int flags) {
+
+  FILE* file = fopen(name, "rb");
+
+  nbt_reader_t reader;
+
+  reader.read = reader_read;
+  reader.userdata = file;
+
+  nbt_tag_t* tag = nbt_parse(reader, flags);
+
+  fclose(file);
+
+  return tag;
+}
+
+void write_nbt_file(const char* name, nbt_tag_t* tag, int flags) {
+
+  FILE* file = fopen(name, "wb");
+
+  nbt_writer_t writer;
+
+  writer.write = writer_write;
+  writer.userdata = file;
+
+  nbt_write(writer, tag, flags);
+
+  fclose(file);
+}
 
 int main() {
     std::cout << "start";
+    nbt_tag_t* tag = read_nbt_file("Test.litematic", NBT_PARSE_FLAG_USE_GZIP);
+    print_nbt_tree(tag, 2);
+    
 }
