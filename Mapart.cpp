67ceb8939fcb1323e8b124976ca3816a55e83b50
polarbub <bryn.hakl@gmail.com>
2021-03-12 12:36:14 -0600

    More things

--- Mapart.cpp
+++ Mapart.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include "nbt2.hpp"
+#include "nbt.hpp"
+
+using namespace nbt;
 
 int main() {
-
+	std::cout << "1";
+    std::ifstream input("Test.litematic");
+	std::cout << "1";
+    nbt::tags::compound_tag root;
+	std::cout << "2";
+    input >> nbt::contexts::java >> root;
+	std::cout << "3";
+    std::ofstream output("mojangson.txt");
+	std::cout << "4";
+    output << nbt::contexts::mojangson << root;
+	std::cout << "5";
 }
